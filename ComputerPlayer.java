f9b3d4e654adeed762384bedabac163d77b21a2b
dvanleusen <danvanleusen@yahoo.co.uk>
2016-10-14 04:22:49 -0600

    1st refactoring - refused bequest smell
    1st refactoring - refused bequest smell, relevant methods were pushed
    down to their corresponding subclass

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -10,7 +10,7 @@
 * @version Feb 23, 2015
 */
 
-public class ComputerPlayer extends Player {
+public class ComputerPlayer {
 	
 	public Score computerScore;
 
@@ -20,5 +20,36 @@ public class ComputerPlayer extends Player {
 	public ComputerPlayer(int cpuScore) {
 		computerScore = new Score();
 		computerScore.setScore(cpuScore);
+	}
+
+	/**
+	 * The method play() is called by the Controller after each die roll.
+	 * The scores do not change in the middle of a turn.
+	 * Only at the end of a turn, the Controller updates the scores.
+	 *  
+	 * @param	computerScore	the computer player score at the beginning of this turn 
+	 * @param	humanScore		the human player score at the beginning of this turn
+	 * @param	points			the sum of rolls collected so far in this turn
+	 * @return	the next move of the player as a character: Roll or Hold	
+	 */
+	public char computer(int computerScore, int humanScore, int points) {
+
+		char compDecision = ' ';
+		// checks if either player's score is over 71, if yes, it will roll
+		if (humanScore >= 71 || computerScore >= 71) {
+			compDecision = 'r';
+		}
+		// otherwise it will determine the base value and roll until it is achieved
+		else {
+			int baseComputer = (int) 21+ Math.round((computerScore - humanScore)/8);
+			if (points < baseComputer) {
+				compDecision = 'r';
+			}
+			
+			else {
+				compDecision = 'h';
+			}
+		}
+		return compDecision;
 	}
 }
--- HumanPlayer.java
+++ HumanPlayer.java
@@ -15,10 +15,10 @@ import java.util.Scanner;
 * @version Feb 23, 2015
 */
 
-public class HumanPlayer extends Player  {
+public class HumanPlayer {
 
 	public Score humanScore;
-
+	private int baseHuman = -1;
 
 	/**
 	 * Constructor for creating an interactive human player.
@@ -34,6 +34,38 @@ public class HumanPlayer extends Player  {
 			baseHuman = base;
 		}
 	}
-	
 
+	
+	/**
+	 * The method play() is called by the Controller after each die roll.
+	 * The scores do not change in the middle of a turn.
+	 * Only at the end of a turn, the Controller updates the scores.
+	 *  
+	 * @param	points			the sum of rolls collected so far in this turn
+	 * @return	the next move of the player as a character: Roll or Hold	
+	 */
+	public char human(int points) {
+		char humanDecision = ' ';
+
+		if (baseHuman == -1) {
+			Scanner keyboard = new Scanner(System.in);
+			// prompts user to choose hold or roll
+			while (humanDecision != 'h' && humanDecision != 'r') {
+				System.out.print("     H: (r)un or (h)old? ");
+				humanDecision = keyboard.next().charAt(0);
+			}
+		}
+		// automated mode: roll until the user entered hold base value is achieved
+		else {
+			if (points < baseHuman)	{
+				humanDecision = 'r';
+			}
+
+			else {
+				humanDecision = 'h';
+			}
+		}
+
+		return humanDecision;
+	}
 }
